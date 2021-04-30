4e733844ee10563b74a2e76c51c4c23945f5c6d0
Julian Berman <Julian@GrayVines.com>
2021-04-30 17:11:30 -0400

    Optional semicolons and intro.

--- grammar/tactic.js
+++ grammar/tactic.js
@@ -1,7 +1,10 @@
 module.exports = {
-  tactics: $ => prec.left(seq('by', sep1_($._tactic, $._newline))),
+  tactics: $ => prec.left(
+    seq('by', sep1_($._tactic, seq(optional(';'), $._newline))),
+  ),
 
   apply_tactic: $ => seq('apply', $._expression),
+  intro: $ => seq('intro', $.identifier),
   rewrite: $ => seq(choice('rewrite', 'rw'), $._expression),
   term: $ => seq('exact', $._expression),
   simp: $ => prec.right(seq(
@@ -14,6 +17,7 @@ module.exports = {
 
   _tactic: $ => choice(
     $.apply_tactic,
+    $.intro,
     $.rewrite,
     $.simp,
     $.term,
