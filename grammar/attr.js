a360c1c655a08179164da14e4e8c5a2d8a0a28fe
Julian Berman <Julian@GrayVines.com>
2021-08-11 11:49:27 +0100

    Add support for extern.

--- grammar/attr.js
+++ grammar/attr.js
@@ -1,9 +1,23 @@
 // src/Lean/Parser/Attr.lean
 
 module.exports = {
-  _attribute: $ => choice($._simple_attribute),
+  _attribute: $ => choice(
+    $._simple_attribute,
+    $._extern_attribute,
+  ),
 
   _simple_attribute: $ => field(
     'name', seq($.identifier, optional($.identifier)),
   ),
+
+  _extern_entry: $ => seq(
+    optional($.identifier),
+    optional('inline'),
+    field('extern', $.string),
+  ),
+  _extern_attribute: $ => seq(
+    'extern',
+    optional($.number),
+    repeat($._extern_entry),
+  )
 }
